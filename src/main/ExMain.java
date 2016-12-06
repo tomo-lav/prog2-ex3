31fd37f22992e6cb05380f3b63feb4fbbf60dd26
Tomoya Uchihara <e165728@ie.u-ryukyu.ac.jp>
2016-12-06 10:29:32 +0900

    version 1.0

---
+++ src/main/ExMain.java
@@ -0,0 +1,16 @@
+package main;
+
+public class ExMain {
+    public static void main(String[] args){
+        Player player = new Player();
+        Maze maze = new Maze();
+
+        現在位置();
+        左右移動();
+        現在位置();
+        上下移動();
+        現在位置();
+        左右移動();
+        現在位置();
+    }
+}
